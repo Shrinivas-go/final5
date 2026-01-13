0178de990bd023c46bf5ccacd63ae9fc20285a40
Shrinivas-go <shrinivas4927@gmail.com>
2026-01-13 12:43:34 +0530

    new:

--- program5.py
+++ program5.py
@@ -1,14 +1,12 @@
 import sys
-
-if len(sys.argv) == 4:
-  script_name = float(sys.argv[0])
+script_name = sys.argv[0]
+if len(sys.argv) >= 4:
+  print("Usage: python program5.py <principal> <time> <rate>")
+  sys.exit(1)
+else:
   principal = float(sys.argv[1])
   time = float(sys.argv[2])
   rate = float(sys.argv[3])
-else:
-  principal = 10000
-  time = 2
-  rate = 5.22
 
 simple_interest = (principal*time*rate)/100
 print(f"Simple Interest: {simple_interest}")
