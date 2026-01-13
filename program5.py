8dd3e31482bb5d10e32828ccef2730084e1febc0
Shrinivas-go <shrinivas4927@gmail.com>
2026-01-13 13:11:39 +0530

    saving

--- program5.py
+++ program5.py
@@ -1,15 +1,16 @@
 import sys
 
-if len(sys.argv) == 4:
-  script_name = float(sys.argv[0])
-  principal = float(sys.argv[1])
-  time = float(sys.argv[2])
-  rate = float(sys.argv[3])
-else:
-  principal = 10000
-  time = 2
-  rate = 5.22
+if len(sys.argv) != 4:
+    print("Usage: python simple_interest.py <principal> <rate> <time>")
+    sys.exit(1)
 
-simple_interest = (principal*time*rate)/100
-print(f"Simple Interest: {simple_interest}")
+principal = float(sys.argv[1])
+rate = float(sys.argv[2])
+time = float(sys.argv[3])
 
+simple_interest = (principal * rate * time) / 100
+
+print("Principal:", principal)
+print("Rate:", rate)
+print("Time:", time)
+print("Simple Interest:", simple_interest)
